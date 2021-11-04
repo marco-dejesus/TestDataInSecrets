6e0d1cb0e56ff5059d6d5a03955af91aa48de2b5
Marco De Jesus <marco.dejesus@offerup.com>
2021-11-04 15:27:28 -0600

    creating mixed data

--- argumentor.js
+++ argumentor.js
@@ -5,5 +5,30 @@ let users = JSON.parse(arguments[0])
 
 console.log(users);
 
+console.log(users[0]["OUTesting1@email.com"]);
+
+let mixedData = [
+    {
+        "email": "OUTesting1@email.com",
+        "password": users[0]["OUTesting1@email.com"],
+        "testData1" : "testdata",
+        "testData2" : "testdata",
+        "testData3" : "testdata",
+        "testData4" : "testdata",
+    },
+    {
+        "email": "OUTesting3@email.com",
+        "password": users[2]["OUTesting3@email.com"],
+        "testData1" : "testdata",
+        "testData2" : "testdata",
+        "testData3" : "testdata",
+        "testData4" : "testdata",
+    }
+]
+
+//console.log()
+console.log(mixedData)
+//console.log(Object.keys(mixedData[0]))
+
 
 //console.log(arguments); 
