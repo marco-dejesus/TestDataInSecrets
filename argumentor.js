06512db71e44de6c1ac395d7b97342366396d44f
Marco De Jesus <marco.dejesus@offerup.com>
2021-11-05 15:37:26 -0600

    F

--- argumentor.js
+++ argumentor.js
@@ -2,30 +2,32 @@ let arguments = process.argv.slice(2);
 
 //let users = JSON.parse(arguments[0])
 
-console.log(arguments);
+let argu = "{'user1': {'email': 'OUTesting1@email.com', 'password': '12$345'},'user2': {'email': 'OUTesting2@email.com','password': '12$345'},'user3': { 'email': 'OUTesting3@email.com','password': '12$345'},'user4': {  'email': 'OUTesting4@email.com','password': '12$345'}}"
+
+console.log(argu);
 
 
 //console.log(users);    
 
-//console.log(users[0]["OUTesting1@email.com"]);
+//console.log(users[0]['OUTesting1@email.com']);
 
 /*
 let mixedData = [
     {
-        "email": "OUTesting1@email.com",
-        "password": users[0]["OUTesting1@email.com"],
-        "testData1" : "testdata",
-        "testData2" : "testdata",
-        "testData3" : "testdata",
-        "testData4" : "testdata",
+        'email': 'OUTesting1@email.com',
+        'password': users[0]['OUTesting1@email.com'],
+        'testData1' : 'testdata',
+        'testData2' : 'testdata',
+        'testData3' : 'testdata',
+        'testData4' : 'testdata',
     },
     {
-        "email": "OUTesting3@email.com",
-        "password": users[2]["OUTesting3@email.com"],
-        "testData1" : "testdata",
-        "testData2" : "testdata",
-        "testData3" : "testdata",
-        "testData4" : "testdata",
+        'email': 'OUTesting3@email.com',
+        'password': users[2]['OUTesting3@email.com'],
+        'testData1' : 'testdata',
+        'testData2' : 'testdata',
+        'testData3' : 'testdata',
+        'testData4' : 'testdata',
     }
 ]*/
 
