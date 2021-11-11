ebb92562f2761916bb7989b5217edbfb36ec9c67
Marco De Jesus <marco.dejesus@offerup.com>
2021-11-11 11:24:55 -0600

    Updating the format of users

--- data/iteration-data.js
+++ data/iteration-data.js
@@ -1,8 +1,28 @@
 const getIterationData = (testUsers) => {
     return [
         {
-            "email": testUsers.user1.email,
-            "password": testUsers.user1.password
+            "email": testUsers.staging.user1.email,
+            "password": testUsers.staging.user1.password
+        },
+        {
+            "email": testUsers.staging.user2.email,
+            "password": testUsers.staging.user2.password
+        },
+        {
+            "email": testUsers.staging.user3.email,
+            "password": testUsers.staging.user3.password
+        },
+        {
+            "email": testUsers.staging.user4.email,
+            "password": testUsers.staging.user4.password
+        },
+        {
+            "email": testUsers.staging.user5.email,
+            "password": testUsers.staging.user5.password
+        },
+        {
+            "email": testUsers.staging.user6.email,
+            "password": testUsers.staging.user6.password
         }
     ]
 }
